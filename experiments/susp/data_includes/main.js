7f2d007d684b0be0bbba76bac1c7ed2a5bdd7c54
utku turk <utkuturk@umd.edu>
2025-04-13 21:47:01 +0000

    better css

--- experiments/susp/data_includes/main.js
+++ experiments/susp/data_includes/main.js
@@ -1,22 +1,22 @@
 // TODO: 
 // [X] put fillers and exp and practice into same template 
-// [ ] improve text_css to get a value for fontsize
+// [X] improve text_css to get a value for fontsize
 // [X] include yes/no + likert / ARE WE DOING THIS?
-// [ ] improve overall css for presentation. text should not wrap to the window.
+// [X] improve overall css for presentation. text should not wrap to the window.
 // [ ] improve css for the question.
 // [X] make sure that question only pops up for some of the trials
 // [X] add Latin Square Group
-// [ ] correct repeating "devam" or "space"
+// [X] correct repeating "devam" or "space"
 // [ ] Edit intro and explanations
-// [ ] Header completion
-// [ ] Create a completion code and ask for that in the google form
+// [X] Header completion
+// [X] Create a completion code and ask for that in the google form
 // [ ] Create a google form after the experient for them to give their email address
 
 PennController.ResetPrefix();
 PennController.DebugOff();
 PennController.SetCounter("increase")
 
-Sequence("instructions", "intro", "counter", startsWith("practice"), "break", rshuffle("exp", "filler"), "SendResults()");
+Sequence("instructions", "demo", "counter", startsWith("practice"), "break", rshuffle("exp", "filler"), "SendResults()");
 
 var sendingResultsMessage = "Sonuçlarınız gönderiliyor, lütfen bekleyin.";
 var randomnumber = Math.floor(Math.random()*1000000);
@@ -38,7 +38,7 @@ Header(
     newVar("correctAnswer").global()
   )
     // .log("PROLIFIC", GetURLParameter("id"))
-    // .log("completionCode", completionCode)
+    .log("completionCode", completionCode)
     .log("itemNum", getVar("itemNum"))
     .log("trialNum", getVar("trialNum"))
     .log("sentence" ,getVar("sentence"))
@@ -54,7 +54,29 @@ Header(
 var text_css = {
     "font-size": "24px",
     "font-family": "Helvetica, sans-serif"
+};
+
+var inst_text_css = {
+    margin: "0 auto",
+    "font-size": "20px",
+    "font-family": "sans-serif",
   };
+
+  
+var underline_blank = {
+    outline: "none",
+    resize: "none",
+    border: "0",
+    padding: "0",
+    margin: "0",
+    "margin-left": "1ex",
+    "margin-right": "1ex",
+    "vertical-align": "-.33em",
+    "background-color": "white",
+    "border-bottom": "2px solid black",
+    display: "inline",
+};
+
 
 const trialN = () => [
     newVar("TrialN", 0)
@@ -62,25 +84,25 @@ const trialN = () => [
         .set((v) => v + 1)
 ];
 
-const demog = (label, text) => [
-    newText("before", text)
-        .css("font-size","16px")
-        .css("font-family", "Helvetica, sans-serif")
-    ,
+
+const newDemo = (label, text) => [
     newTextInput(label)
-        .before(getText("before"))
-        .css("font-size","16px")
-        .log()
-        .lines(1)
-        .print()
-    ,
-    newVar(label).global().set(getTextInput(label))
+      .before(newText(text).size("15em", "1.5em"))
+      .size("15em", "1.5em")
+      .lines(1)
+      .css(underline_blank)
+      .center()
+      .log()
+      .print(),
 ];
 
 
 const print_sentence = (f) => [
     newText(f)
         .css(text_css)
+        .css("margin", "1.5em")
+        .css("text-align", "center")
+        .settings.size("40em")
         .center()
         .print()
         .log()
@@ -88,37 +110,44 @@ const print_sentence = (f) => [
 
 const print_scale = () => [
     newText("right", "Kesinlikle duyabileceğim/söyleyeceğim bir cümle")
-        .css("font-size","20px")
+        .css("font-size","14px")
         .css("font-family", "Helvetica, sans-serif")
+        .css("margin-left", "2em")
+        .css("margin-right", "1em")
+        .css("margin-top", "2.5em")
         .italic()
     ,
     newText("left", "Kesinlikle duyabileceğim/söyleyeceğim bir cümle değil")
-        .css("font-size","20px")
+        .css("font-size","14px")
         .css("font-family", "Helvetica, sans-serif")
+        .css("margin-left", "1em")
+        .css("margin-top", "2.5em")
         .italic()
     ,
     newScale(7)
         .radio()
-        .labelsPosition("bottom") 
+        .labelsPosition("bottom")
+        .keys()
         .before(getText("left"))
         .after(getText("right"))
-        .css(text_css)
+        .css("font-size","16px")
+        .css("font-family", "Helvetica, sans-serif")
+        .css("margin", "30pt")
+        .cssContainer("border", "solid 1px black")
         .log()
         .center()
         .print()
         .wait() 
 ];
 
-const print_space = (n,label) => {
-    let spaceString = "";
+const print_space = (n, label = "") => {
+    let spaceBlocks = [];
     for (let i = 0; i < n; i++) {
-      spaceString += "<p> </p>";
+        spaceBlocks.push(newText("Space" + label + "_" + i, " ").print());
     }
-    return [
-      newText("Space" + label, spaceString)
-        .print()
-    ];
-  };
+    return spaceBlocks;
+};
+
 
 const devam = (fontsize, label) => [
     newButton("devam"+label, "Devam")
@@ -132,7 +161,7 @@ const devam = (fontsize, label) => [
 SetCounter("counter", "inc", 1)
 
 newTrial("instructions",
-
+    defaultText.css(inst_text_css),
     fullscreen(),
     
     newText(`<p>Merhaba! Size internet ortamında yürütülen bir psikodilbilim deneyi için ulaşıyoruz. Bu deney Massachusetts Üniversitesi Amherst kampüsünden Özge Bakay tarafından yürütülüyor. </p>
@@ -157,78 +186,91 @@ newTrial("instructions",
     devam("16px")
 )
 
-newTrial("intro",
-    demog("Age", "Yaşınız:"),
-    print_space(1, "intro1"),
-    demog("NativeLangauge", "Anadiliniz ya da anadilleriniz:"),
-    print_space(1, "intro2"),
-    demog("DominantLanguage","Birden fazla anadiliniz varsa kendinizi daha rahat ifade ettiğiniz dil:"),
-    print_space(1, "intro3"),    
-    demog("ForeignLanguage", "Konuştuğunuz diğer diller:"),
-    print_space(1, "intro4"),
-    demog("School", "Eğitim durumunuz (lise mezunu, üniversite öğrencisi, üniversite mezunu):"),
-    print_space(1, "intro5"),
-    ////////////////
+
+newTrial(
+    "demo",
+    defaultText.css(inst_text_css),
+    newDemo("age", "Yaşınız*:"),
+    ...print_space(1, "intro1"),
+    newDemo("language", "Anadil(ler)iniz*:"),
+    ...print_space(1, "intro1"),
+    newDemo("otherlg", "Konuştuğunuz diğer diller:"),
+    ...print_space(1, "intro1"),
+    newDemo("school", "Eğitim durumunuz:"),
+    
+    ...print_space(3, "intro6"),
+
     newText("Bilgiler için teşekkürler! Deneye başlamadan önce birkaç alıştırma cümlesi göreceksiniz. Alıştırma cümlelerine geçmek için aşağıdaki butona tıklayın.")
-        .css("font-size","16px")
-        .css("font-family", "Helvetica, sans-serif")
         .print()
     ,
-    print_space(1, "intro6"),
-    devam("16px", "intro")
-)
-
-.log("Age", getVar("Age"))
-.log("NativeLanguage", getVar("NativeLanguage"))
-.log("DominantLanguage", getVar("DominantLanguage"))
-.log("ForeignLanguage", getVar("ForeignLanguage"))
-.log("School", getVar("School"))
+    ...print_space(3, "intro6"),
+    newButton("devam"+"demo", "Devam")
+        .css("font-size", "16px")
+        .css("font-family", "Helvetica, sans-serif")
+        .print()
+        .wait(
+            getTextInput("age")
+                .test.text(/^\d+.+$/)
+                .failure(
+                    newText("Lütfen yaşınızı sayı ile giriniz.").settings.color("red").print()
+                )
+            .and(
+                getTextInput("language")
+                .testNot.text("")
+                .failure(
+                    newText("Lütfen anadilinizi ya da ana dillerinizi giriniz.")
+                    .settings.color("red")
+                    .print()
+                )
+            )
+        ),
+  );
 
 newTrial("practice1",
     print_sentence("Hizmetçi merdiveni duvara yasladı ama bu pek güvenli değildi."),
-    print_space(4, "prac1"),
+    ...print_space(4, "prac1"),
     print_scale(),
-    print_space(1, "prac2"),
+    ...print_space(1, "prac2"),
     devam("24px"),
-    print_space(4, "prac3"),
+    ...print_space(4, "prac3"),
     newText("Örneğin, Türkçe konuşanlar bu cümleye genelde 6 ya da 7 gibi yüksek puanlar veriyor.")
         .css(text_css)
         .center()
         .print()
     ,
-    // print_space(1),
+    ...print_space(1),
     devam("24px", "prac")
 )
 
 newTrial("practice2",
 print_sentence("Köyü ziyaret eden belediye başkanı asla beğenmiş."),
-    print_space(4, "prac21"),
+    ...print_space(4, "prac21"),
     print_scale(),
-    print_space(1, "prac22"),
+    ...print_space(1, "prac22"),
     devam("24px"),
-    print_space(4, "prac23"),
+    ...print_space(4, "prac23"),
     newText("Örneğin, Türkçe konuşanlar bu cümleye genelde 1 ya da 2 gibi düşük puanlar veriyor.")
         .css(text_css)
         .center()
         .print()
     ,
-    print_space(1, "prac24"),
+    ...print_space(1, "prac24"),
     devam("24px", "prac2")
 )
 
 newTrial("practice3",
     print_sentence("Harabeler onarılmaya dün çalışıldı."),
-    print_space(4, "prac31"),
+    ...print_space(4, "prac31"),
     print_scale(),
-    print_space(1, "prac32"),
+    ...print_space(1, "prac32"),
     devam("24px"),
-    print_space(4, "prac33"),
+    ...print_space(4, "prac33"),
     newText("Türkçe konuşanlar bu cümleye 3 ya da 4 gibi arada puanlar veriyor.")
         .css(text_css)
         .center()
         .print()
     ,
-    print_space(1, "prac34"),
+    ...print_space(1, "prac34"),
     devam("24px", "prac3")
 )
 
@@ -238,7 +280,7 @@ newTrial("break",
         .print()
         .css(text_css)
     ,
-    print_space(2, "break"),
+    ...print_space(2, "break"),
     devam("24px", "break")
 )
 
@@ -248,9 +290,9 @@ var trial = (label) => (row) => {
         label,
         trialN(),
         print_sentence(row.sentence),
-        print_space(4, "trialbase1"),
+        ...print_space(4, "trialbase1"),
         print_scale(),
-        print_space(1, "trialbase2"),
+        ...print_space(1, "trialbase2"),
         devam("24px", "trialbase"),
         
         newVar("is_question", "").set(row.isq),
@@ -262,7 +304,7 @@ var trial = (label) => (row) => {
                     .center()
                     .print()
                     .log(),
-                print_space(1, "q1"),
+                ...print_space(1, "q1"),
                 newVar("is_correct").global().set(false),
                 newScale("answer", "Evet", "Hayir")
                     .checkbox()
@@ -274,7 +316,7 @@ var trial = (label) => (row) => {
                 getScale("answer")
                     .test.selected(row.correct_answer)
                     .success(getVar("is_correct").set(true)),
-                print_space(1, "q2"),
+                ...print_space(1, "q2"),
                 newButton("devam" + "q", "Devam")
                     .css(text_css)
                     .center()
